bec8f073f80771f0ad3af3c2953d6a59002d4949
Алексей Сорокин <alecsey.m.s@gmail.com>
2021-06-22 12:10:05 +0300

    updates : 22.06.2021

--- temp.py
+++ temp.py
@@ -1,20 +1,31 @@
 import json
 
+
+def findall(v, k):
+    if type(v) == type({}):
+        for k1 in v:
+            if k1 == k:
+                print(v[k1])
+                return v[k1]
+            result = findall(v[k1], k)
+            if result is not None:
+                return result
+
+    if type(v) == type([]):
+        for k1 in v:
+            result = findall(k1, k)
+            if result is not None:
+                return result
+
+
 with open('c:/testing/response2.json') as json_file:
     data = json.load(json_file)
     data_str = json.dumps(data)
 
-print(data)
-print(data_str)
+    results = findall(data, "WinStateInfo")
 
-userSavedState = data['SpinResult']['UserSavedState']['StepPlayerContracts'][1]['WinStateInfo']
-print(userSavedState)
-
-
-if 'WinStateInfo' in data_str:
-    print('OK')
-else:
-    print('missing ...')
-
+    print(results["WinState"])
+    print(results["FreeSpinCount"])
+    print(results["Multiplier"])
 
 # print(json.dumps(data, indent=2))
